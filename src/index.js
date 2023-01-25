e32ec1294ccd710087f8861a90e1544e645477d3
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-25 22:31:22 +0800

    Add /list boilerplate

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const { Octokit } = require("@octokit/core");
+const {
+    restEndpointMethods,
+} = require("@octokit/plugin-rest-endpoint-methods");
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient()
-
+const MyOctokit = Octokit.plugin(restEndpointMethods);
+const octokit = new MyOctokit({ auth: process.env.GITHUB_PAT });
 
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post(`/register`, async (req, res) => {
+app.post(`/api/register`, async (req, res) => {
+    // Must return JWT
     const { email, password } = req.body
 
     const result = await prisma.users.create({
@@ -23,9 +25,23 @@ app.post(`/register`, async (req, res) => {
         },
     })
     res.json(result)
-    console.log(email, password)
 })
 
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body
+
+    // res.json(jwt)
+})
+
+app.get('/api/list', async (req, res) => {
+    // const { data: { login } } = await octokit.rest.users.getByUsername({
+    // const { data } = await octokit.rest.users.getByUsername({
+    const { data: { name, login, company, followers, public_repos } } = await octokit.rest.users.getByUsername({
+        username: "ro11ingbutler"
+    });
+    console.log({ name, login, company, followers, public_repos, avg_follower_per_repo: followers / public_repos })
+    res.json(`Hello`)
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
