31466a4c832dfd923e607c49c706f03352a4b2e6
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-26 09:05:06 +0800

    Correct the GraphQL query

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const { PrismaClient } = require('@prisma/client');
-const { Octokit } = require("@octokit/core");
-const {
-    restEndpointMethods,
-} = require("@octokit/plugin-rest-endpoint-methods");
+// const { Octokit } = require("@octokit/core");
+// const {
+//     restEndpointMethods,
+// } = require("@octokit/plugin-rest-endpoint-methods");
+
+const { graphql } = require("@octokit/graphql");
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient()
-const MyOctokit = Octokit.plugin(restEndpointMethods);
-const octokit = new MyOctokit({ auth: process.env.GITHUB_PAT });
+// const MyOctokit = Octokit.plugin(restEndpointMethods);
+// const octokit = new MyOctokit({ auth: process.env.GITHUB_PAT });
+
+const graphqlWithAuth = graphql.defaults({
+    headers: {
+        authorization: `bearer ${process.env.GITHUB_PAT}`,
+    },
+});
 
 app.use(express.json())
 
@@ -33,15 +41,51 @@ app.post('/api/login', async (req, res) => {
     // res.json(jwt)
 })
 
+// app.get('/api/__list', async (req, res) => {
+//     // const { data: { login } } = await octokit.rest.users.getByUsername({
+//     // const { data } = await octokit.rest.users.getByUsername({
+//     const { data: { name, login, company, followers, public_repos } } = await octokit.rest.users.getByUsername({
+//         username: "ro11ingbutler"
+//         // }, {
+//         //     username: "jjjjcccjjf"
+//     });
+//     console.log({ name, login, company, followers, public_repos, avg_follower_per_repo: followers / public_repos })
+//     console.log(data)
+//     res.json(`Hello`)
+// })
+
 app.get('/api/list', async (req, res) => {
-    // const { data: { login } } = await octokit.rest.users.getByUsername({
-    // const { data } = await octokit.rest.users.getByUsername({
-    const { data: { name, login, company, followers, public_repos } } = await octokit.rest.users.getByUsername({
-        username: "ro11ingbutler"
+
+    const data = await graphqlWithAuth({
+        query: `query SearchUsers($login: String!) {
+            search(query: $login, type: USER, first: 10) {
+              edges {
+                node {
+                  ... on User {
+                    name
+                    login
+                    company
+                    repositories(privacy: PUBLIC) {
+                      totalCount
+                    }
+                    followers {
+                      totalCount
+                    }
+                  }
+                }
+              }
+            }
+          }`,
+        login: "ro11ingbutler OR jjjjcccjjf"
     });
-    console.log({ name, login, company, followers, public_repos, avg_follower_per_repo: followers / public_repos })
-    res.json(`Hello`)
+
+ 
+    console.log(data)
+
+    res.json(data)
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
