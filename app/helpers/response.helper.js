2da61031624ab5e8ac3955a4fae86667ff424962
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-27 15:25:06 +0800

    Add documentation for helpers

--- app/helpers/response.helper.js
+++ app/helpers/response.helper.js
@@ -1,4 +1,11 @@
+/**
+ *
+ * @param {object} data - Data from redis or github API we put in the `data` key
+ * @param {string} errors - Error message
+ * @returns {object} - Returns an object containing the response template
+ */
 function responseTemplate (data, errors) {
+  // block of code for counting the response data
   let dataLength = 0
   if (data instanceof Array) {
     dataLength = data.length
@@ -10,6 +17,7 @@ function responseTemplate (data, errors) {
     }
   }
 
+  // return our new structure for the response
   return {
     data,
     meta: {
@@ -19,6 +27,12 @@ function responseTemplate (data, errors) {
   }
 }
 
+/**
+ *
+ * @param {object} data - The default data structure from the API
+ * @param {bool} fromCache - Specifies if the data is from the redis cache or not
+ * @returns {object} - Returns the data in our preferred format
+ */
 function formatData (data, fromCache = false) {
   return {
     name: data.name,
