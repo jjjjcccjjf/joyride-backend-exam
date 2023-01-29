81c35b47be3bc91e15635f332983ed29c1d2b24e
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-29 13:56:11 +0800

    Add more descriptive error for the errorHandler

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const responseHelper = require('../helpers/response.helper')
 
 module.exports = (err, req, res, next) => {
-  return res.status(400).json(responseHelper.responseTemplate({}, err.type))
+  return res.status(400).json(responseHelper.responseTemplate({}, err.type === 'entity.parse.failed' ? 'Illegal JSON format for request payload' : err.type))
 }
